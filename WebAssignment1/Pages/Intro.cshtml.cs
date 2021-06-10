66bf57cffe6787985f0a89dab9231a12b9b70ecc
aakashsadnani <A00235504@mycambrian.ca>
2021-06-11 05:24:08 +0530

    Initial commit

---
+++ WebAssignment1/Pages/Intro.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebAssignment1.Pages
+{
+    public class IntroModel : PageModel
+    {
+        public void OnGet()
+        {
+
+                result = InputNumber1 * InputNumber2;
+  
+        }
+
+        [FromQuery(Name = "InputNumber1")]
+
+        public int InputNumber1 { get; set; }
+
+        [FromQuery(Name = "InputNumber2")]
+
+        public int InputNumber2 { get; set; }
+
+        public int result { get; set; }
+
+        
+    }
+}
