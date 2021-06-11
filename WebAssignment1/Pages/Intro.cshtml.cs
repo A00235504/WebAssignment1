7a51491b916e057821b5d2192e4105bd3ef8afbc
aakashsadnani <A00235504@mycambrian.ca>
2021-06-11 05:43:27 +0530

    Modified the files for adding usefull comments

--- WebAssignment1/Pages/Intro.cshtml.cs
+++ WebAssignment1/Pages/Intro.cshtml.cs
@@ -11,19 +11,24 @@ namespace WebAssignment1.Pages
     {
         public void OnGet()
         {
-
+                //performing calculations on the two inputs
+                //saving the result in result variable
                 result = InputNumber1 * InputNumber2;
   
         }
-
+        //getting data from the Input1
         [FromQuery(Name = "InputNumber1")]
 
+        //creating a variable of int type for input1
         public int InputNumber1 { get; set; }
 
+        //getting data from the Input2 for input2
         [FromQuery(Name = "InputNumber2")]
 
+        //creating a variable of int type
         public int InputNumber2 { get; set; }
 
+        //creating a variable of int type for result
         public int result { get; set; }
 
         
